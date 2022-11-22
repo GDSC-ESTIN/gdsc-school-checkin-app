688c2ae5d0f08962e60b94a85463b7a1d60d5bdb
aliouche mohamed <m_aliouche@estin.dz>
2022-11-22 20:21:00 +0100

    react part

---
+++ scanner/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Scanner } from './pages/Scann';
+function App() {
+  return (
+    <div className="App">
+      <Scanner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ scanner/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {Scanner} from './pages/Scann'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Scanner/>
+);
